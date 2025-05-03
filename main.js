e2211f3477e5b49b1aa892c9da384f97b2378d42
roberthouse38 <monkmalone14@gmail.com>
2025-05-03 21:40:39 +0700

    add contact

--- main.js
+++ main.js
@@ -14,7 +14,29 @@ function toggleDetail(e){
     const detail = $(item).children(".about-exp-item-detail")
 
     $(detail).slideToggle()
-
-
     // console.log($(item).children(".about-exp-item-detail")) # Untuk debugging hierarki child atau parent
+}
+
+// UNTUK MENAMPILKAN POP UP ALERT DARI javascript 
+// (lebih aman daripada menambahkan 'required' pada html )
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    // console.log($(subject).val()) UNTUK DEBUG MASUKAN
+
+    if(!$(email).val()) {
+        alert("Email Is Required")
+    } else if (!$(subject).val()) {
+        alert("Subject Is Required")
+    } else if (!$(message).val()) {
+        alert("Message Is Required")
+    } else {
+        alert("form submitted")
+        $(email).val(" ")
+        $(subject).val(" ")
+        $(message).val(" ")
+    }
 }
